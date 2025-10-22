c89e790848d45c9462d4001c9d45b1ccbb6ad981
Kashif J. Ford <ford_kashif@hotmail.com>
2025-10-21 21:47:50 -0500

    project initialization

---
+++ src/main/java/com/pawnee/parks/crm/ParksCrmApplication.java
@@ -0,0 +1,13 @@
+package com.pawnee.parks.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParksCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawnee/parks/crm/ParksCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawnee.parks.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
