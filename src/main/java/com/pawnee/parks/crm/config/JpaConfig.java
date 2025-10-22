beeaad5d6dad579e00f7690dabc99a431f27d6cd
Kashif J. Ford <ford_kashif@hotmail.com>
2025-10-21 21:49:42 -0500

    Added config file to enable auditing

---
+++ src/main/java/com/pawnee/parks/crm/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.pawnee.parks.crm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
